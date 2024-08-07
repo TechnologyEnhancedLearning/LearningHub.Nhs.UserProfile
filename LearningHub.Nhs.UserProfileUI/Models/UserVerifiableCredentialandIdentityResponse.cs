b91018bde86f96ee83434f6673e466bbbe7a00fa
Oluwatobi Awe <Oluwatobi.Awe@hee.nhs.uk>
2024-08-07 12:20:25 +0100

    TD-4345 step 6 updates

---
+++ LearningHub.Nhs.UserProfileUI/Models/UserVerifiableCredentialandIdentityResponse.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="UserVerifiableCredentialandIdentityResponse.cs" company="HEE.nhs.uk">
+// Copyright (c) HEE.nhs.uk.
+// </copyright>
+
+namespace LearningHub.Nhs.UserProfileUI.Models
+{
+    using LearningHub.Nhs.LearningCredentials.Models.Dsp;
+
+    /// <summary>
+    /// Definition of the user verifiable credential response and identity status.
+    /// </summary>
+    public class UserVerifiableCredentialandIdentityResponse
+    {
+        /// <summary>
+        /// Gets or sets UserVerifiableCredentialResponse.
+        /// </summary>
+        public List<UserVerifiableCredentialResponse>? UserVerifiableCredentialResponse { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether user identity has been verified.
+        /// </summary>
+        public bool IdentityVerified { get; set; } = false;
+    }
+}
