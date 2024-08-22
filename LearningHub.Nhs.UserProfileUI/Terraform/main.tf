83bb2bb9554f0e699dd7fe413b2b25ead15f98e8
Colin Beeby <colin.beeby@hee.nhs.uk>
2024-08-22 15:30:12 +0100

    Change from using linux web app to windows web app

--- LearningHub.Nhs.UserProfileUI/Terraform/main.tf
+++ LearningHub.Nhs.UserProfileUI/Terraform/main.tf
@@ -8,10 +8,23 @@ resource "azurerm_service_plan" "UserProfileServicePlan" {
   location            = azurerm_resource_group.UserProfileResourceGroup.location
   resource_group_name = azurerm_resource_group.UserProfileResourceGroup.name
   sku_name			  = "B1"
-  os_type			  = "Linux"
+  os_type			  = "Windows"
 }
 
-resource "azurerm_linux_web_app" "UserProfileLinuxWebApp" {
+/*resource "azurerm_linux_web_app" "UserProfileLinuxWebApp" {
+  name                = var.WebAppName
+  location            = azurerm_resource_group.UserProfileResourceGroup.location
+  resource_group_name = azurerm_resource_group.UserProfileResourceGroup.name
+  service_plan_id     = azurerm_service_plan.UserProfileServicePlan.id
+  site_config {
+	app_command_line  = "dotnet Learninghub.Nhs.UserProfileUI.dll"
+	application_stack {
+	  dotnet_version = "6.0"
+	}
+  }
+}*/
+
+resource "azurerm_windows_web_app" "UserProfileLinuxWebApp" {
   name                = var.WebAppName
   location            = azurerm_resource_group.UserProfileResourceGroup.location
   resource_group_name = azurerm_resource_group.UserProfileResourceGroup.name
